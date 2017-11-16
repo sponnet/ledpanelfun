f284f64f53a22e0066c85049521d9c72fc3add5f
Stefaan Ponnet <stefaan@ponnet.com>
2017-11-16 18:26:04 +0100

    ethinfo

---
+++ ethinfo.js
@@ -0,0 +1,79 @@
+const request = require('request');
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/TyQelmM18AzWHSMN0GBP'));
+
+setInterval(function() {
+
+	web3.eth.getBlockNumber()
+		.then(function(bn) {
+			console.log('ETH blockheight', bn);
+			var line2="";
+			if (price_swt){
+				line2+="SWT/EUR " + price_swt.price_eur + " ";
+			}
+			if (price_eth){
+				line2+="ETH/EUR " + price_eth.price_eur + " ";
+			}
+			console.log(line2);
+		});
+
+}, 3000);
+
+
+getPrice();
+
+var price_swt;
+var price_eth;
+
+setInterval(function() {
+	getPrice();
+},60*1000);
+
+
+function getPrice() {
+
+	request('https://api.coinmarketcap.com/v1/ticker/swarm-city/?convert=EUR',
+		(error, response, body) => {
+			if (error || (response && response.statusCode !== 200)) {
+				return
+			}
+			try {
+				let parsedBody = JSON.parse(body);
+				price_swt = {
+					price_btc: parsedBody[0].price_btc,
+					price_eur: Number(parsedBody[0].price_eur).toFixed(2),
+					price_usd: Number(parsedBody[0].price_usd).toFixed(2),
+					symbol: parsedBody[0].symbol,
+				};
+				console.log(price);
+				//logger.info('updateFx result=', result);
+				//return resolve(result);
+			} catch (e) {
+				//logger.error('updateFx err=', e);
+				return;
+			}
+		});
+
+	request('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR',
+		(error, response, body) => {
+			if (error || (response && response.statusCode !== 200)) {
+				return
+			}
+			try {
+				let parsedBody = JSON.parse(body);
+				price_eth = {
+					price_btc: parsedBody[0].price_btc,
+					price_eur: Number(parsedBody[0].price_eur).toFixed(2),
+					price_usd: Number(parsedBody[0].price_usd).toFixed(2),
+					symbol: parsedBody[0].symbol,
+				};
+				console.log(price);
+				//logger.info('updateFx result=', result);
+				//return resolve(result);
+			} catch (e) {
+				//logger.error('updateFx err=', e);
+				return;
+			}
+		});
+
+}
